c928f2facced393ed0ff48f9522caa352ec613ed
vklysoff@gmail.com <vklysoff@gmail.com@fcf80903-91b9-66df-9600-9ee4faba29b2>
2011-04-25 12:58:42 +0000

    Prepared for new CR

--- tags/CR/pbs/user/examples/firefly.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh!
-cd firefly
-~/submit-tp -i /usr/local/firefly/samples/condircbk.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j firefly -v mpich
-~/submit-tp -i ~/examples/firefly/exam01.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j firefly -v mpich
-cd -
