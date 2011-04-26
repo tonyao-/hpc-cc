70ee9bb9d0420f940af32b75745527ce2e61e53d
vklysoff <vklysoff@gmail.com>
2011-04-26 09:25:32 +0000

    Fix some scripts

--- pbs/user/examples/firefly.sh
+++ pbs/user/examples/firefly.sh
@@ -1,5 +1,5 @@
 #/bin/sh!
 cd firefly
-~/submit-tp -i /usr/local/firefly/samples/condircbk.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j firefly -v mpich
-~/submit-tp -i ~/examples/firefly/exam01.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j firefly -v mpich
+~/submit-tp -q short -i /usr/local/firefly/samples/condircbk.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j ff_condircbk -v mpich
+~/submit-tp -q short -i ~/examples/firefly/exam01.inp -n 4 -d /usr/local/firefly/mpich-dyn -f /usr/local/firefly/mpich-dyn/firefly -j ff_exam01 -v mpich
 cd -
--- pbs/user/examples/g03.sh
+++ pbs/user/examples/g03.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd g03
-~/submit-tp -n 2 -f ~/submit-g03.sh -i ~/examples/g03/test164.com -o ~/examples/g03/test164-n2-l.log
-~/submit-tp -n 4 -f ~/submit-g03.sh -i ~/examples/g03/test164.com -o ~/examples/g03/test164-n4-l.log
-~/submit-tp -n 2 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test164.com -o ~/examples/g03/test164-n2.log
-~/submit-tp -n 4 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test164.com -o ~/examples/g03/test164-n4.log
-~/submit-tp -n 2 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test691.com -o ~/examples/g03/test691-n2.log
-~/submit-tp -n 4 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test691.com -o ~/examples/g03/test691-n4.log
-~/submit-tp -n 2 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test695.com -o ~/examples/g03/test695-n2.log
-~/submit-tp -n 4 -f ~/submit-g03.sh -i /usr/local/g03/tests/com/test695.com -o ~/examples/g03/test695-n4.log
+~/submit-tp -q short -n 2 -f ~/submit-g03.sh -j g03_test164-n2-l -i ~/examples/g03/test164.com -o ~/examples/g03/test164-n2-l.log
+~/submit-tp -q short -n 4 -f ~/submit-g03.sh -j g03_test164-n4-l -i ~/examples/g03/test164.com -o ~/examples/g03/test164-n4-l.log
+~/submit-tp -q short -n 2 -f ~/submit-g03.sh -j g03_test164-n2 -i /usr/local/g03/tests/com/test164.com -o ~/examples/g03/test164-n2.log
+~/submit-tp -q short -n 4 -f ~/submit-g03.sh -j g03_test164-n4 -i /usr/local/g03/tests/com/test164.com -o ~/examples/g03/test164-n4.log
+~/submit-tp -q short -n 2 -f ~/submit-g03.sh -j g03_test691-n2 -i /usr/local/g03/tests/com/test691.com -o ~/examples/g03/test691-n2.log
+~/submit-tp -q short -n 4 -f ~/submit-g03.sh -j g03_test691-n4 -i /usr/local/g03/tests/com/test691.com -o ~/examples/g03/test691-n4.log
+~/submit-tp -q short -n 2 -f ~/submit-g03.sh -j g03_test695-n2 -i /usr/local/g03/tests/com/test695.com -o ~/examples/g03/test695-n2.log
+~/submit-tp -q short -n 4 -f ~/submit-g03.sh -j g03_test695-n4 -i /usr/local/g03/tests/com/test695.com -o ~/examples/g03/test695-n4.log
 cd -
