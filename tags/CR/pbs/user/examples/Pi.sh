6a6216277b15379c2e06ace32d6b1d174543f740
vklysoff@gmail.com <vklysoff@gmail.com@fcf80903-91b9-66df-9600-9ee4faba29b2>
2011-08-24 13:30:57 +0000

    Prepared for new Current Release

--- tags/CR/pbs/user/examples/Pi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd Pi
-~/submit-tp -f ~/examples/Pi/Pi.comp-openmpi.sh -d ~/examples/Pi
-~/submit-tp -v openmpi -j Pi -n 16 -f ~/examples/Pi/Pi.openmpi
-cd -
--- tags/CR/pbs/user/user/examples/Pi/Pi.comp-openmpi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export prefix="/usr/lib64/openmpi/1.4-gcc"
-
-export PATH=$prefix/bin:$PATH
-export LD_LIBRARY_PATH=$prefix/lib:$LD_LIBRARY_PATH
-mpif77 Pi.f -o Pi.openmpi
-
