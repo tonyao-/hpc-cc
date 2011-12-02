1457717dce05eb333ad72a638a200ea45b05d1e6
vklysoff@gmail.com <vklysoff@gmail.com@fcf80903-91b9-66df-9600-9ee4faba29b2>
2011-12-02 16:56:57 +0000

    Prepared for new Current Release

--- tags/CR/pbs/user/examples/firefly.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh!
-cd firefly
-~/submit-tp -q short -i /usr/local/firefly/samples/condircbk.inp -n 4 -d /usr/local/firefly/mpich2 -f /usr/local/firefly/mpich2/firefly -j ff_condircbk -v mpich2
-~/submit-tp -q short -i ~/examples/firefly/exam01.inp -n 4 -d /usr/local/firefly/mpich2 -f /usr/local/firefly/mpich2/firefly -j ff_exam01 -v mpich2
-cd -
