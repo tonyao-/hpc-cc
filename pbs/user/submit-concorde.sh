12f3a008478dbad7947c22fc5cb0a353cc559c29
vklysoff <vklysoff@gmail.com>
2012-06-19 13:10:41 +0000

    refs #628

--- pbs/user/submit-concorde.sh
+++ pbs/user/submit-concorde.sh
@@ -3,7 +3,7 @@
 i=1
 for node in `cat $PBS_NODEFILE`; do
   echo node=$node i=$i
-  ssh $node "~/examples/concorde/concorde-script.sh $i" &
+  ssh $node "~/examples/concorde-script.sh $i" &
   let i=i+1
 done
 wait
