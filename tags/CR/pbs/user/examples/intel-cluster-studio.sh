e3b60b74049059641b1e66f40508c94b17ef422f
vklysoff@gmail.com <vklysoff@gmail.com@fcf80903-91b9-66df-9600-9ee4faba29b2>
2012-06-19 10:57:04 +0000

    Delete old CR

--- tags/CR/pbs/user/examples/intel-cluster-studio.sh
+++
@@ -1,9 +0,0 @@
-# Intel Cluster Studio
-ICS_PREFIX=/usr/local/ICS
-. $ICS_PREFIX/bin/iccvars.sh intel64
-. $ICS_PREFIX/impi/4.0.3.008/bin64/mpivars.sh
-. $ICS_PREFIX/itac/8.0.3.007/bin/itacvars.sh
-. $ICS_PREFIX/vtune_amplifier_xe/amplxe-vars.sh
-. $ICS_PREFIX/icsxe/2012.0.032/inspector_xe/inspxe-vars.sh
-export IDB_HOME=$ICS_PREFIX/composer_xe_2011_sp1.6.233/bin/intel64
-
--- tags/CR/pbs/user/examples/molpro/h2.com
+++
@@ -1,6 +0,0 @@
-        ***,H2
-        file,2,h2.wf,new;
-        punch,h2.pun;
-        basis=vdz;
-        geometry={angstrom;h1;h2,h1,.74}
-        hf
