e40907ce54bebd27ca1448a1e05f34a62421e816
gvladimiru@gmail.com <gvladimiru@gmail.com@fcf80903-91b9-66df-9600-9ee4faba29b2>
2012-11-27 13:18:28 +0000

    Delete old CR

--- tags/CR/pbs/user/examples/run-all.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh!
-~/submit-tp -f ~/examples/helloworld.sh
-~/submit-tp -f ~/examples/intel.sh
-~/submit-tp -f ~/examples/Pi/Pi.comp-openmpi.sh -d ~/examples/Pi
-~/submit-tp -v openmpi -j Pi-6 -n 4 -f ~/examples/Pi/Pi.openmpi
-~/examples/firefly.sh
-~/examples/g03.sh
--- tags/CR/pbs/user/examples/test_nag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Specify licence server
-export NAG_KUSARI_FILE=192.168.0.58:
-
-# Check version
-/usr/local/NAG/nagfor -V
-
